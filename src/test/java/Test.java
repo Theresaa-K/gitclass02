5a929cc83596c8a71432604cd782cd8c255ff8e3
Theresaa-K <kennytheresa7@gmail.com>
2024-11-05 16:37:13 -0600

    firstcommit

---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This is project version 1");
+    }
+}
+//done with version 1
