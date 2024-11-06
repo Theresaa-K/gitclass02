4cc5f6518408e5bf9af5f478c44dd74e61f1ebca
Theresaa-K <kennytheresa7@gmail.com>
2024-11-06 10:02:47 -0600

    fourth commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,11 +1,11 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("This is project version 3");
-        System.out.println("after our practice we will take a break");
-        System.out.println("you can grab a drink");
-        System.out.println("get some food ");
-        System.out.println("or play the One Piece card game!");
+        System.out.println("This is project version 4");
+        System.out.println("we will continue our lesson");
+        System.out.println("grab a drink if you need");
+        System.out.println("Adjust your seat so you are comfortable");
+        System.out.println("good luck with the rest of the lesson");
     }
 }
-//done with version 3
+//done with version 4
