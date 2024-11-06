09c32b04df0fb684d3b9c0bbee686b13f7e6b387
Theresaa-K <kennytheresa7@gmail.com>
2024-11-06 08:18:08 -0600

    second commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,11 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("This is project version 1");
+        System.out.println("This is project version 2");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
-//done with version 1
+//done with version 2
